347b494154417c73794c983da1245874a308e7ba
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2022-11-15 15:57:20 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { HeaderMegaMenu } from '../component/HeaderMegaMenu';
+import { HeroContentLeft } from '../component/HeroContentLeft';
+import Layout from './../component/layout';
+import LayoutPages from '../component/layoutpage';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps, router } = props;
+  // props: Componet load, các thông số của page vừa load
+  const getLayout = router.pathname.includes('/auth') ? ((page: any) => <Layout children={page} />)
+    : ((page: any) => <LayoutPages children={page} />);
+
+  return (
+    <>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
+  
 }
+export const getServerSideProps = (async function ({ req, res }) {
+  const { user } = req.session
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { user },
+  }
+})
+
