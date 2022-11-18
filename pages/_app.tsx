dac43473fa8e4bec3adfd747ed971231a6bf07ae
ckem1st997 <hopxc1997@gmail.com>
2022-11-18 19:40:33 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,10 +41,18 @@ export default function App(props: AppProps) {
   //props: Componet load, các thông số của page vừa load
   const getLayout = router.pathname.includes('/auth') ? ((page: any) => <Layout>{page}</Layout>)
     : ((page: any) => <LayoutPages>{page}</LayoutPages>);
-    const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
-    const toggleColorScheme = (value?: ColorScheme) =>
-      setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-  
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  // phần hiển thị trên google sẽ show trong thẻ head
+  // phần header và footer (nếu có) dự tính sẽ sử dụng client side rendering và dạng tĩnh, 
+  // có cache để tránh call api nhiều lần
+  // trang quản trị thì 100% là client side rendering vì không cần SEO
+  // các trang page bình thường để xem nội dung thì 100% là serve side rendering
+  // trang nào mà api ít thay đổi hoặc không thay đổi thì có thể dùng getStaticProps với revalidate tuỳ ý
+  // trang nào mà hay dùng api, các trang detalis, login, check role thì có thể dùng getServeProps để real time api
+  // chú ý phần các key phân quyền, sẽ check nếu key không hoạt động thì được phép truy cập, còn hoạt động thì mới check quyền
+
   return (
     <>
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
